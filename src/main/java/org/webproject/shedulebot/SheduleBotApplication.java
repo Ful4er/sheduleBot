51e3dc6328c855668c02734043dbb131da5d6902
Ful4er <daniilwork211@gmail.com>
2025-06-14 21:34:47 +0300

    initial commit

---
+++ src/main/java/org/webproject/shedulebot/SheduleBotApplication.java
@@ -0,0 +1,13 @@
+package org.webproject.shedulebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheduleBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SheduleBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/webproject/shedulebot/SheduleBotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.webproject.shedulebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheduleBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
